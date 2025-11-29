47a9d4e3716d24a0211881b05608fd3e4ff446e1
Tatiana-Kolomeichuk <tany663@gmail.com>
2025-11-29 10:56:32 +0000

    Deploying to gh-pages from @ Tatiana-Kolomeichuk/Blended_Products@25558075f01a0dbd99ee9eaa008a09f4920de8b2 🚀

---
+++ index.js
@@ -0,0 +1,2 @@
+import"./assets/styles-JE8YjOlG.js";import{a as t}from"./assets/vendor-2s9xPmg-.js";const o="https://dummyjson.com",a={PRODUCTS:"/products",PRODUCTS_BY_ID:"/products/",CATEGORIES:"/products/category-list",SEARCH:"/products/search"};t.defaults.baseURL=o;async function c(){const{data:s}=await t.get(a.CATEGORIES);return s}c();
+//# sourceMappingURL=index.js.map
