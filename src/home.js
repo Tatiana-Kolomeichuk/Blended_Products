e5eff4200c42e9fd226a7d0411ca9cbb7979744d
Aїda <izograf.zinaida@gmail.com>
2025-12-06 00:26:31 +0200

    feature/delegate-category-list

--- src/home.js
+++ src/home.js
@@ -1,9 +1,8 @@
 //Логіка сторінки Home
-import { initHomePage } from "./js/handlers";
-import { closeModal, openModal } from "./js/modal";
-import { refs } from "./js/refs";
+import { handleCategoryClick, initHomePage } from './js/handlers';
+import { closeModal, openModal } from './js/modal';
+import { refs } from './js/refs';
 
-document.addEventListener("DOMContentLoaded",initHomePage)
+document.addEventListener('DOMContentLoaded', initHomePage);
 refs.closeModalBtn.addEventListener('click', closeModal);
-
-
+refs.categoriesList.addEventListener('click', handleCategoryClick);
--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,14 +1,54 @@
-import { getCategories, getProducts } from "./products-api";
-import { renderCategories, renderProducts } from "./render-function";
+import { STATE } from './constants';
+import {
+  clearProductsList,
+  hideNotFoundBlock,
+  showNotFoundBlock,
+  updateActiveCategory,
+} from './helpers';
+import {
+  getCategories,
+  getProducts,
+  getProductsByCategory,
+} from './products-api';
+import { renderCategories, renderProducts } from './render-function';
 
 export async function initHomePage() {
+  try {
+    const categories = await getCategories();
+    renderCategories(categories);
+    const { products } = await getProducts(STATE.PAGE);
+    renderProducts(products);
+  } catch (error) {}
+}
+
+export async function handleCategoryClick(e) {
+  const btnEl = e.target.closest('.categories__btn');
+  if (!btnEl) {
+    return;
+  }
+  clearProductsList();
+  updateActiveCategory(btnEl);
+  hideNotFoundBlock();
+  STATE.PAGE = 1;
+  const query = btnEl.textContent;
+  if (query === 'All') {
     try {
-        const categories = await getCategories();
-        renderCategories(categories);
-        const {products}= await getProducts();
-        renderProducts(products);
-        
-    } catch (error) {
-        
+      const { products } = await getProducts(STATE.PAGE);
+      renderProducts(products);
+    } catch (err) {
+      console.error(`Get products error: ${err}`);
     }
+  } else {
+    STATE.QUERY = query;
+    try {
+      const { products } = await getProductsByCategory(STATE.QUERY, STATE.PAGE);
+      if (products.length === 0) {
+        showNotFoundBlock();
+      } else {
+        renderProducts(products);
+      }
+    } catch (err) {
+      console.error(`Get products error: ${err}`);
+    }
+  }
 }
