ef92ee5cd90732081c1e4279c2decc11f89579be
Tatiana-Kolomeichuk <tany663@gmail.com>
2025-12-15 16:50:25 +0000

    Deploying to gh-pages from @ Tatiana-Kolomeichuk/Blended_Products@9b860682f26140050f19206850c3e84aef500882 🚀

--- cart.js
+++ cart.js
@@ -1,2 +1,2 @@
-import"./assets/styles-JE8YjOlG.js";import{r as e,f as t,g as a,j as r,k as d}from"./assets/handlers-Btmru1dd.js";import"./assets/vendor-EEf_bTFo.js";e.productsList.addEventListener("click",t);e.buyBtnCart.addEventListener("click",a);e.modal.addEventListener("click",t);e.searchForm.addEventListener("submit",r);d();
+import"./assets/styles-JE8YjOlG.js";import{r as e,f as t,g as a,j as d,k as n}from"./assets/handlers-BJSltFHN.js";import"./assets/vendor-EEf_bTFo.js";e.productsList.addEventListener("click",t);e.buyBtnCart.addEventListener("click",a);document.addEventListener("DOMContentLoaded",d);e.modal.addEventListener("click",t);e.searchForm.addEventListener("submit",n);
 //# sourceMappingURL=cart.js.map
